e4df392685b2b40661597fe5fbb06c62d4ce59f7
Daria Mayorova <d.mayorova@gmail.com>
2016-03-03 12:19:52 +0100

    Add CORS

--- app/randomizr_api.rb
+++ app/randomizr_api.rb
@@ -1,16 +1,25 @@
 require 'sinatra/base'
 require 'sinatra/namespace'
 require 'sinatra/param'
+require 'sinatra/cross_origin'
 require 'json'
 
 class RandomizrApi < Sinatra::Base
 
   register Sinatra::Namespace
+  register Sinatra::CrossOrigin
   helpers Sinatra::Param
 
   configure do
     enable :logging
+    enable :cross_origin
   end
+
+  set :allow_origin, :any
+  set :allow_methods, [:get, :post, :options]
+  set :allow_credentials, true
+  set :max_age, "1728000"
+  set :expose_headers, ['Content-Type']
 
   # configure :development do
   #   set :raise_sinatra_param_exceptions, true
@@ -190,6 +199,12 @@ class RandomizrApi < Sinatra::Base
       { message: "Locale '#{params[:code]}' saved successfully" }.to_json
     end
 
+  end
+
+  options "*" do
+    response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+    200
   end
 
   # A 404 error is sent when trying to access any endpoint that is not implemented yet
